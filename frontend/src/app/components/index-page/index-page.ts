66709253ba32d1727d92cb6610d3aaa78cf38533
Niculai Ilie-Traian <niculaii58@gmail.com>
2026-05-24 12:59:30 +0300

    docker configuration

--- frontend/src/app/components/index-page/index-page.ts
+++ frontend/src/app/components/index-page/index-page.ts
@@ -54,23 +54,9 @@ export class IndexPage {
 				{ name: 'Eficiență', textValue: '10.2 l/100km', score: 40 }
 			]
 		},
-{
-            id: 'm2',
-            rank: 2,
-            name: 'Mercedes-Benz AMG GT',
-            imageUrl: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?w=800&q=80',
-            rating: 4.8,
-            reviewCount: 320,
-            specs: [
-                { name: 'Performanță', textValue: '367 CP', score: 85},
-                { name: 'Accelerație', textValue: '4.8s (0-100)', score: 75 },
-                { name: 'Confort', textValue: 'Piele Nappa & MBUX', score: 98},
-                { name: 'Consum Mixt', textValue: '8.3 L/100km', score: 65 }
-            ]
-        },
 		{
-			id: 'm3',
-			rank: 3,
+			id: 'm2',
+			rank: 2,
 			name: 'Audi RS6 Avant',
 			imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/2023-audi-rs-6-avant-performance111-6499d2d01c180.jpg?crop=0.567xw:0.424xh;0.335xw,0.429xh&resize=1400:*',
 			rating: 4.9,
@@ -80,6 +66,20 @@ export class IndexPage {
 				{ name: 'Spațiu Portbagaj', textValue: '565 Litri', score: 85 },
 				{ name: 'Tracțiune', textValue: 'Quattro AWD', score: 100 },
 				{ name: 'Confort', textValue: 'Pneumatică', score: 90 }
+			]
+		},
+		{
+			id: 'm3',
+			rank: 3,
+			name: 'Mercedes-Benz AMG GT',
+			imageUrl: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?w=800&q=80',
+			rating: 4.8,
+			reviewCount: 320,
+			specs: [
+				{ name: 'Performanță', textValue: '367 CP', score: 85 },
+				{ name: 'Accelerație', textValue: '4.8s (0-100)', score: 75 },
+				{ name: 'Confort', textValue: 'Piele Nappa & MBUX', score: 98 },
+				{ name: 'Consum Mixt', textValue: '8.3 L/100km', score: 65 }
 			]
 		}
 	]);
